7408165fa178a44799e153741df56b2c2fc42810
Ubuntu <azureuser@Ubuntu-ARM64-2.rbelrextlsxu5p5fyrq50l3p4d.cx.internal.cloudapp.net>
2025-06-16 11:32:52 +0000

    updating latest package

--- idp/ums/configuration/onpremise/boldid/postgresql/versioned_scripts/latest.sql
+++ idp/ums/configuration/onpremise/boldid/postgresql/versioned_scripts/latest.sql
@@ -1,7 +1,18 @@
-CREATE TABLE IF NOT EXISTS BOLDTC_BackUp (
-    Id SERIAL primary key NOT NULL,
-    ConfigurationData text NOT NULL,
-    PrivateKey text NOT NULL,
-    ModifiedDate timestamp NOT NULL,
-    IsActive smallint NOT NULL
+CREATE TABLE BOLDTC_TenantStorageDetails (
+    Id uuid NOT NULL,
+    TenantInfoId uuid NOT NULL,
+    StorageType INT NOT NULL,
+    ConnectionInfo VARCHAR(1026),
+    CreatedDate TIMESTAMP NOT NULL,
+    ModifiedDate TIMESTAMP NOT NULL,
+    IsActive smallint NOT NULL,
+    CONSTRAINT PK_BOLDTC_TenantStorageDetails PRIMARY KEY (Id)
 );
+
+ALTER TABLE BOLDTC_TenantInfo ADD COLUMN IF NOT EXISTS StorageType INT NOT NULL DEFAULT 0;
+
+UPDATE BOLDTC_TenantInfo
+SET StorageType = 1
+WHERE StorageType = 0
+  AND BlobConnectionString IS NOT NULL
+  AND BlobConnectionString <> '';
